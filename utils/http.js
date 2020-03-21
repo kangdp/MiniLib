d718026c998309869458d09924c93a7847969a6d
kangdongpu <1452398128@qq.com>
2020-03-21 16:10:07 +0800

    修改 http.js

--- utils/http.js
+++ utils/http.js
@@ -2,24 +2,24 @@
 
 const doRequest = (method) => {
 
-  return function (obj) {
+  return function(obj) {
     if (!obj || !obj.url) return
     var contentType = method == 'GET' ? 'application-json' : 'application/x-www-form-urlencoded'
     if (obj.contentType) {
       contentType = obj.contentType
     }
     var data = {}
-    if(obj.data) {
-		data = obj.data
-	}
-    
+    if (obj.data) {
+      data = obj.data
+    }
+
     wx.request({
       url: obj.url,
       method: method,
       header: {
         'content-type': contentType
       },
-	  data:data,
+      data: data,
       success: (res) => {
         if (obj.success) {
           obj.success(res)
