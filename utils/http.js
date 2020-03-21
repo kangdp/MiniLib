4dfd925c36107cb14fe94931e3c6a77514224bed
kangdongpu <1452398128@qq.com>
2020-03-21 16:04:12 +0800

    修改http.js

--- utils/http.js
+++ utils/http.js
@@ -1,36 +1,40 @@
 //对 get、post请求进行封装
 
-const doRequest = (method)=> {
-  
+const doRequest = (method) => {
+
   return function (obj) { //闭包
-    if(!obj || !obj.url) return
-    var contentType = method == 'GET' ? 'application-json' :  'application/x-www-form-urlencoded'
-    if(obj.contentType){
+    if (!obj || !obj.url) return
+    var contentType = method == 'GET' ? 'application-json' : 'application/x-www-form-urlencoded'
+    if (obj.contentType) {
       contentType = obj.contentType
     }
+    var data =[]
+    if(obj.data) data = obj.data
+    
     wx.request({
-      url: encodeURI(obj.url),
-      method:method,
+      url: obj.url,
+      method: method,
+      data:data,
       header: {
-        'content-type':contentType
+        'content-type': contentType
       },
       success: (res) => {
-        if(obj.success){
+        if (obj.success) {
           obj.success(res)
         }
       },
       fail: () => {
-        if(obj.fail){
+        if (obj.fail) {
           obj.fail()
         }
       },
       complete: () => {
-        if(obj.complete){
+        if (obj.complete) {
           obj.complete()
         }
       }
     })
-  } 
+  }
 
 }
 
